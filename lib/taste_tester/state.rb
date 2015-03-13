540ede926cbd5ab552283506411d0cd067a06302
Marcin Sawicki <odcinek@fb.com>
2015-03-13 15:43:30 -0700

    Style check

--- lib/taste_tester/state.rb
+++ lib/taste_tester/state.rb
@@ -1,13 +1,13 @@
 # vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
 
 # Copyright 2013-present Facebook
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,7 +30,7 @@ module TasteTester
 
     def initialize
       ref_dir = File.dirname(File.expand_path(
-        TasteTester::Config.ref_file
+                               TasteTester::Config.ref_file
       ))
       unless File.directory?(ref_dir)
         begin
@@ -93,7 +93,7 @@ module TasteTester
 
     def wipe
       if TasteTester::Config.ref_file &&
-          File.exists?(TasteTester::Config.ref_file)
+         File.exists?(TasteTester::Config.ref_file)
         File.delete(TasteTester::Config.ref_file)
       end
     end
